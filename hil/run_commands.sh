898248c50ba3cae6155bf69853af54c4236ca342
Timothy-Flavin <binaryowlizard@gmail.com>
2026-07-07 22:30:00 -0500

    run update

--- hil/run_commands.sh
+++ hil/run_commands.sh
@@ -1,6 +1,6 @@
 
 HIL
-python main.py --env LunarLander-v3 --algo cql --offline_rl --online_rl --awbc --intervention --num_rl_frames 2000 --num_unified_epochs 200 --preload_expert_data "expert_demonstrations_LunarLander-v3.pkl" --experiment_name "2_interactive_hil"
+python main.py --env LunarLander-v3 --algo cql --offline_rl --online_rl --awbc --intervention --num_rl_frames 2000 --num_unified_epochs 500 --preload_expert_data "expert_demonstrations_LunarLander-v3.pkl" --experiment_name "2_interactive_hil" --num_iterations 20
 
 
 HIL + CUR
